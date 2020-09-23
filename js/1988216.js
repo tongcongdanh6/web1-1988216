0c60c3f7d64298b13b5af044db578e54a4b7af4b
unknown <tong.congdanh@gmail.com>
2020-09-24 01:16:49 +0700

    API gallery and add wow.js

--- js/1988216.js
+++ js/1988216.js
@@ -1,3 +1,5 @@
+new WOW().init();
+
 function getData(request, targetid, sourceid) {
     $.ajax({
         url: 'http://web1-api.herokuapp.com/api/' + request,
@@ -13,4 +15,17 @@ function getData(request, targetid, sourceid) {
             $(target).html(template(jsonData));
         }
     });
+}
+
+
+getGallery = (e, id) => {
+    if(id) {
+        getData(`gallery/categories/${id}`, '#gallery', '#gallery-template');
+    }
+    else {
+        getData('gallery', '#gallery', '#gallery-template');
+    }
+    $('#gallery-categories .nav-link').removeClass('disabled');
+
+    $(e).addClass('disabled');
 }
