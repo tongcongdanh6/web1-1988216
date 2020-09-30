9fcc02d460c66063daf7d2faac3015e5ed563bf2
unknown <tong.congdanh@gmail.com>
2020-09-30 13:06:47 +0700

    get API readmore for Categories and Archives and build pagination

--- js/1988216.js
+++ js/1988216.js
@@ -1,4 +1,13 @@
-new WOW().init();
+function initializeWOW_Effect() {
+    new WOW().init();
+}
+
+/*
+Handlebars Helper Register
+*/
+Handlebars.registerHelper('ifEqual', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
 function getData(request, targetid, sourceid) {
     $.ajax({
@@ -37,8 +46,38 @@ getDetailsOfGallery = (id) => {
     }
 }
 
-getBlogDetails = (id) => {
-    if(id) {
-        
+getBlogs = (request, curPage = 1) => {
+    $.ajax({
+        url: `http://web1-api.herokuapp.com/api/${request}?page=${parseInt(curPage)}`,
+        cache: false,
+        success: function (data) {
+            console.log(data);
+            let jsonData = {
+                data
+            };
+            let target = $("#blogs");
+            let source = $("#blogs-template").html();
+            let template = Handlebars.compile(source);
+            $(target).html(template(jsonData));
+
+            jsonData = {
+                currentPage: parseInt(curPage),
+                pageCount: data.pageCount,
+                size: 2,
+                api: request
+            }
+
+            target = $("#blogs-pagination");
+            source = $("#blogs-pagination-template").html();
+            template = Handlebars.compile(source);
+            $(target).html(template(jsonData));
+        }
+    });
+}
+
+getBlogDetail = (request) => {
+    console.log(request);
+    if(request) {
+        getData(`blogs/${request}`, '#blogs', '#blogs-readmore-template');
     }
 }
